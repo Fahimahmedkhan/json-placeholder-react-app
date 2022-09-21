f2059a6d56d516b0249f564010a64220d7e9c495
Fahimahmedkhan <fahimkhangorbo@yahoo.com>
2022-09-22 00:47:22 +0600

    Footer Added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='absolute text-center inset-x-0 bottom-0'>
+            <p>Coded and maintained with ❤️ by <a href="https://github.com/typicode" target="_blank" rel="noopener noreferrer">typicode</a> © 2021</p>
+        </div>
+    );
+};
+
+export default Footer;
