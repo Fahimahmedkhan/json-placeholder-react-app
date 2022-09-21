ea99443976c027a282353796907b703809978d49
Fahimahmedkhan <fahimkhangorbo@yahoo.com>
2022-09-22 01:10:11 +0600

    Banner Added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='w-full h-96 bg-blue-800 text-white text-center flex flex-col space-y-10'>
+            <h1 className='text-6xl mt-10'>JSON Placeholder</h1>
+            <h2 className='text-3xl'>Free fake API for testing and prototyping.</h2>
+            <h2 className='text-3xl'>Powered by JSON Server + LowDB. Tested with XV.</h2>
+            <h3 className='text-xl'>As of Oct 2021, serving ~1.7 billion requests each month.</h3>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='absolute text-center inset-x-0 bottom-0'>
+        <div className='absolute text-center inset-x-0 bottom-0 mb-5'>
             <p>Coded and maintained with ❤️ by <a href="https://github.com/typicode" target="_blank" rel="noopener noreferrer">typicode</a> © 2021</p>
         </div>
     );
